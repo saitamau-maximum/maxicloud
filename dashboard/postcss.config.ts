45bebf79786a3158872363ae512c0dd647f77c90
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2026-06-13 23:42:39 +0900

    chore: vite proxy の設定

---
+++ dashboard/postcss.config.ts
@@ -0,0 +1,6 @@
+import pandacss from "@pandacss/dev/postcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [pandacss, autoprefixer],
+};
--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,14 +1,11 @@
-import pandacss from "@pandacss/dev/postcss";
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [pandacss, autoprefixer],
-    },
-  },
   plugins: [reactRouter(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [".localtest.me"],
+  },
 });
